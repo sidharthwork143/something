26b5e8a83f86329bd9e51dcd8f9e4717164fa2cc
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2025-05-03 18:05:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 # Quote and pattern
 QUOTE = "“Work hard in silence, let your success make the noise.” 🚀"
-PATTERN = re.compile(r'(@\w+|https?://\S+)', re.IGNORECASE)
+PATTERN = re.compile(r'(@\w+|https?://\S+\t.me\www)', re.IGNORECASE)
 
 # /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
