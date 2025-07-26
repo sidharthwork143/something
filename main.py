e9136ba5c082aa86517530fabfc9caa999686757
sidharthwork143 <sidharthwork143@gmail.com>
2025-07-26 17:15:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ QUOTE = "“Work hard in silence, let your success make the noise.” 🚀"
 
 # Pattern to detect links/usernames
 PATTERN = re.compile(
-    r'(@\w+|https?://\S+|t\.me/\S+|facebook\.com/\S+|instagram\.com/\S+|youtube\.com/\S+|whatsapp\.com/\S+|www\.\S+|bit\.ly)',
+    r'(@\w+|https?://\S+|t\.me/\S+|facebook\.com/\S+|instagram\.com/\S+|youtube\.com/\S+|whatsapp\.com/\S+|www\.\S+|bit\.ly/S+|cutt\.ly)',
     re.IGNORECASE
 )
 
